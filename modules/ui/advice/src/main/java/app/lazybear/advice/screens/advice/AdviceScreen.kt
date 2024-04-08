30a116fe8853feb85b6d0d15b04acb46c90356e5
Serhii Kovernyk <26122518+Nimolee@users.noreply.github.com>
2024-04-08 13:51:24 +0300

    Hide bottom bar on scroll.

--- modules/ui/advice/src/main/java/app/lazybear/advice/screens/advice/AdviceScreen.kt
+++ modules/ui/advice/src/main/java/app/lazybear/advice/screens/advice/AdviceScreen.kt
@@ -1,5 +1,11 @@
 package app.lazybear.advice.screens.advice
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.FastOutLinearInEasing
+import androidx.compose.animation.core.LinearOutSlowInEasing
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.slideInVertically
+import androidx.compose.animation.slideOutVertically
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
@@ -14,6 +20,8 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListState
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Icon
@@ -23,6 +31,11 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
@@ -49,46 +62,60 @@ fun AdviceScreen(
     viewModel: AdviceViewModel = koinViewModel(),
 ) {
     val bottomInset = WindowInsets.navigationBars.asPaddingValues().calculateBottomPadding()
+    val listState = rememberLazyListState()
+
     Scaffold(
         bottomBar = {
-            Row(
-                verticalAlignment = Alignment.CenterVertically,
-                modifier = Modifier
-                    .background(MaterialTheme.colorScheme.background)
-                    .padding(
-                        bottom = bottomInset + 12.dp,
-                        top = 12.dp,
-                        start = 16.dp,
-                        end = 16.dp,
-                    )
+            AnimatedVisibility(
+                visible = listState.isScrollingUp(),
+                enter = slideInVertically(
+                    initialOffsetY = { fullHeight -> 2 * fullHeight },
+                    animationSpec = tween(durationMillis = 150, easing = LinearOutSlowInEasing)
+                ),
+                exit = slideOutVertically(
+                    targetOffsetY = { fullHeight -> 2 * fullHeight },
+                    animationSpec = tween(durationMillis = 250, easing = FastOutLinearInEasing)
+                ),
             ) {
-                val loadingState = viewModel.loadingFlow.collectAsState(false)
-                OutlinedButton(
-                    onClick = { onPreferencesOpen() },
-                    contentPadding = PaddingValues(0.dp),
-                    enabled = loadingState.value.not(),
-                    modifier = Modifier.size(40.dp),
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    modifier = Modifier
+                        .background(MaterialTheme.colorScheme.background)
+                        .padding(
+                            bottom = bottomInset + 12.dp,
+                            top = 12.dp,
+                            start = 16.dp,
+                            end = 16.dp,
+                        )
                 ) {
-                    Icon(
-                        painter = painterResource(id = R.drawable.ic_profile_18),
-                        contentDescription = null,
-                    )
-                }
-                Spacer(modifier = Modifier.width(12.dp))
-                OutlinedButton(
-                    onClick = { viewModel.surprise() },
-                    enabled = loadingState.value.not(),
-                    modifier = Modifier.weight(1f)
-                ) {
-                    Text(text = stringResource(id = Localization.surprise_me_button))
-                }
-                Spacer(modifier = Modifier.width(12.dp))
-                Button(
-                    onClick = { viewModel.shuffle() },
-                    enabled = loadingState.value.not(),
-                    modifier = Modifier.weight(1f)
-                ) {
-                    Text(text = stringResource(id = Localization.shuffle_button))
+                    val loadingState = viewModel.loadingFlow.collectAsState(false)
+                    OutlinedButton(
+                        onClick = { onPreferencesOpen() },
+                        contentPadding = PaddingValues(0.dp),
+                        enabled = loadingState.value.not(),
+                        modifier = Modifier.size(40.dp),
+                    ) {
+                        Icon(
+                            painter = painterResource(id = R.drawable.ic_profile_18),
+                            contentDescription = null,
+                        )
+                    }
+                    Spacer(modifier = Modifier.width(12.dp))
+                    OutlinedButton(
+                        onClick = { viewModel.surprise() },
+                        enabled = loadingState.value.not(),
+                        modifier = Modifier.weight(1f)
+                    ) {
+                        Text(text = stringResource(id = Localization.surprise_me_button))
+                    }
+                    Spacer(modifier = Modifier.width(12.dp))
+                    Button(
+                        onClick = { viewModel.shuffle() },
+                        enabled = loadingState.value.not(),
+                        modifier = Modifier.weight(1f)
+                    ) {
+                        Text(text = stringResource(id = Localization.shuffle_button))
+                    }
                 }
             }
         }
@@ -96,10 +123,11 @@ fun AdviceScreen(
         val movieState = viewModel.movieFlow.collectAsState(null)
         movieState.value?.let { movie ->
             LazyColumn(
+                state = listState,
                 horizontalAlignment = Alignment.CenterHorizontally,
                 contentPadding = PaddingValues(
                     top = 16.dp + insets.calculateTopPadding(),
-                    bottom = 16.dp + insets.calculateBottomPadding(),
+                    bottom = 16.dp + bottomInset,
                 ),
                 modifier = Modifier.fillMaxWidth(),
             ) {
@@ -168,4 +196,22 @@ fun AdviceScreen(
             }
         }
     }
+}
+
+@Composable
+private fun LazyListState.isScrollingUp(): Boolean {
+    var previousIndex by remember(this) { mutableIntStateOf(firstVisibleItemIndex) }
+    var previousScrollOffset by remember(this) { mutableIntStateOf(firstVisibleItemScrollOffset) }
+    return remember(this) {
+        derivedStateOf {
+            if (previousIndex != firstVisibleItemIndex) {
+                previousIndex > firstVisibleItemIndex
+            } else {
+                previousScrollOffset >= firstVisibleItemScrollOffset
+            }.also {
+                previousIndex = firstVisibleItemIndex
+                previousScrollOffset = firstVisibleItemScrollOffset
+            }
+        }
+    }.value
 }
