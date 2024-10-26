d3072ee7869582b30b1a362dd5d31135012f7a27
Serhii Kovernyk <26122518+Nimolee@users.noreply.github.com>
2024-10-26 11:15:04 +0300

    Fix text lignment of genres selection card

--- modules/ui/components/src/main/java/app/lazybear/module/ui/components/cards/GenreSelectionCard.kt
+++ modules/ui/components/src/main/java/app/lazybear/module/ui/components/cards/GenreSelectionCard.kt
@@ -14,6 +14,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 
 @Composable
@@ -47,6 +48,7 @@ fun GenreSelectionCard(
         Text(
             title,
             style = MaterialTheme.typography.labelSmall,
+            textAlign = TextAlign.Center,
         )
     }
 }
