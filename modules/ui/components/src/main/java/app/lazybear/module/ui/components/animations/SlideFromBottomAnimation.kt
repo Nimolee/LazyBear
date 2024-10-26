a09c56397dd29d5bd0a938779b72485dbd5f6915
Serhii Kovernyk <26122518+Nimolee@users.noreply.github.com>
2024-10-26 11:11:23 +0300

    Add fade in to animations.

--- modules/ui/components/src/main/java/app/lazybear/module/ui/components/animations/SlideFromBottomAnimation.kt
+++ modules/ui/components/src/main/java/app/lazybear/module/ui/components/animations/SlideFromBottomAnimation.kt
@@ -4,11 +4,15 @@ import androidx.compose.animation.AnimatedContentTransitionScope
 import androidx.compose.animation.EnterTransition
 import androidx.compose.animation.core.EaseIn
 import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
 import androidx.navigation.NavBackStackEntry
 
 fun AnimatedContentTransitionScope<NavBackStackEntry>.slideFromBottomAnimation(): EnterTransition {
     return slideIntoContainer(
         AnimatedContentTransitionScope.SlideDirection.Up,
+        animationSpec = tween(300, easing = EaseIn),
+        initialOffset = { (it * 0.2).toInt() }
+    ) + fadeIn(
         animationSpec = tween(300, easing = EaseIn),
     )
 }
--- modules/ui/components/src/main/java/app/lazybear/module/ui/components/animations/SlideToBottomAnimation.kt
+++ modules/ui/components/src/main/java/app/lazybear/module/ui/components/animations/SlideToBottomAnimation.kt
@@ -4,11 +4,15 @@ import androidx.compose.animation.AnimatedContentTransitionScope
 import androidx.compose.animation.ExitTransition
 import androidx.compose.animation.core.EaseIn
 import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeOut
 import androidx.navigation.NavBackStackEntry
 
 fun AnimatedContentTransitionScope<NavBackStackEntry>.slideToBottomAnimation(): ExitTransition {
     return slideOutOfContainer(
         AnimatedContentTransitionScope.SlideDirection.Down,
+        animationSpec = tween(300, easing = EaseIn),
+        targetOffset = { (it * 0.2).toInt() }
+    ) + fadeOut(
         animationSpec = tween(300, easing = EaseIn),
     )
 }
